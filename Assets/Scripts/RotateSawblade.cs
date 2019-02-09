ef9a6b492b994b25267570ae1edaed8a73555da7
wn83 <warren.j.zhou@gmail.com>
2019-02-09 09:39:39 -0800

    Add Sawblade

---
+++ Assets/Scripts/RotateSawblade.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSawblade : MonoBehaviour {
+	GameObject sawblade;
+	void Start() {
+		sawblade = GameObject.Find("Sawblade");
+	}
+	public void rotate(float rs) {
+		sawblade.transform.Rotate(Vector3.forward * -rs);
+	}
+}
---
+++ Assets/Scripts/Sawblade.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sawblade : MonoBehaviour {
+
+	RotateSawblade rotateSawblade;
+	public float SawbladeSpeed;
+	void Start () {
+		rotateSawblade = gameObject.GetComponent(typeof(RotateSawblade)) as RotateSawblade;
+	}
+	
+	void Update () {
+		rotateSawblade.rotate(SawbladeSpeed);
+	}
+}
