8698cc88a9219c113033247a3dcd3fdedd2375c7
wn83 <warren.j.zhou@gmail.com>
2019-02-16 16:14:26 -0800

    Death to Player

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+	public LayerMask Bad;
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.layer == 9) {
+			Die();
+		}
+	}
+	bool FallOutOfWorld () {
+		return transform.position.y < -30;
+	}
+	void DeathEffect() {
+
+	}
+	void Die () {
+		DeathEffect();
+		transform.position = new Vector2(0, 0);	
+		Debug.Log("Die");
+	}
+	void Update () {
+		if (FallOutOfWorld()) {
+			Die();
+		}
+	}
+}
